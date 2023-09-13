a0402f48890d92af66a511d0922bbcbb9399dd42
Martinezisaac <isaac.martinez6437@alumnos.udg.mx>
2023-09-13 14:59:17 -0600

    feat(Main)

---
+++ src/proyectointegrador_equipo10/ProyectoIntegrador_Equipo10.java
@@ -0,0 +1,21 @@
+/*
+Programacion de bajo nivel
+Proyecto Integrador | Parte 1
+
+    Integrantes de este codigo 
+    -Martinez Isaac
+    -Hernandez Gutierrez Emmanuel
+    -Jimenez Castellanos Alex
+*/
+
+package proyectointegrador_equipo10;
+
+//Librerias
+
+public class ProyectoIntegrador_Equipo10 {
+
+    public static void main(String[] args) {
+        // Aqui empieza la magia
+    } //Fin de main 
+    
+} //Fin de la clase
