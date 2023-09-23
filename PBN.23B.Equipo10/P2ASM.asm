ac7eb7851f5a48d316b68ff8985b71da3162edcd
Isaac Martínez <98341452+Martinezisaac@users.noreply.github.com>
2023-09-23 01:12:47 -0600

    [P2] (Chore) -> Integracion del archivo P2ASM

---
+++ PBN.23B.Equipo10/P2ASM.asm
@@ -0,0 +1,25 @@
+	ORG 	$4040
+Et1: 	SWI 
+dos: 	EORA 	5 
+	EORA 	@5 
+Tres: 	EORA 	%111 
+	EORA 	$6 
+	EORA 	300 
+	EORA 	$FFF 
+	EORA 	#5 
+	EORA 	#@5 
+	EORA 	#1500 
+	EORA 	1,X IDX 
+Uno: 	EORA 	255,X 
+	EORA 	32768,X 
+	EORA 	1,+PC IDX 
+	EORA 	A,X IDX 
+	EORA 	254,X 
+	EORA 	64444,X 
+	EORA 	[1,X] 
+	EORA 	[6444,X]
+	EORA 	[D,X] 
+	BLT 	Uno 
+	LBLT 	Uno 
+	IBNE 	A, Uno 
+	END
