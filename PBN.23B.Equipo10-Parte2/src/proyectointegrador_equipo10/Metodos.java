0501ab6b7e45637a17cb365260bf263d503960c8
Martinezisaac <isaac.martinez6437@alumnos.udg.mx>
2023-09-23 23:27:15 -0600

    [P2] (feat) -> Clase metodos

--- PBN.23B.Equipo10-Parte2/src/proyectointegrador_equipo10/Metodos.java
+++ PBN.23B.Equipo10-Parte2/src/proyectointegrador_equipo10/Metodos.java
@@ -4,3 +4,9 @@ package proyectointegrador_equipo10;
 public class Metodos {
     
 } //Fin de la clase metodos 
+//Clase de metodos
+package proyectointegrador_equipo10;
+
+public class Metodos {
+    
+} //Fin de la clase metodos 
