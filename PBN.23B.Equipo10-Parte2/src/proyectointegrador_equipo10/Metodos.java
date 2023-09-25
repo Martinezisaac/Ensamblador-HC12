c3a1b2cca7e8cb92f2561a9c3a1a3fe6cd039b89
Martinezisaac <isaac.martinez6437@alumnos.udg.mx>
2023-09-24 20:33:09 -0600

    [P2] (feat) -> Metodos para convertir de cualquier sistema a decimal

--- PBN.23B.Equipo10-Parte2/src/proyectointegrador_equipo10/Metodos.java
+++ PBN.23B.Equipo10-Parte2/src/proyectointegrador_equipo10/Metodos.java
@@ -6,8 +6,31 @@ import java.util.regex.Pattern;
 
 public class Metodos {
     
+    public static boolean ComprobarEtiqueta(String etiqueta) {
+        if(etiqueta.matches("[a-zA-Z][a-zA-Z0-9_]{0,7}")) {
+            return true; 
+        } //Fin de if          
+        else {
+            return true;  
+        } //Fin de else 
+    } //Fin de compribar etiqueta
+    
+    /*
+    public static void DetermineBits(int ValorDecimal) {
+        if (ValorDecimal >= 0 && ValorDecimal <= 255) {
+            System.out.println("8 bits");
+        } //Fin de if
+        else if (ValorDecimal >= 256 && ValorDecimal <= 65535) {
+            System.out.println("16 bits");
+        } //Fin de else if
+        else {
+            System.out.println("Fuera de rango");
+        } //Fin de else 
+    } //Fin de la funcion para determinar bits 
+    */
+    
     //Funcion para validar si operando es Binario 
-    public static Boolean IsBinario(String binario) {
+    public static Boolean IsBinario(String binario) {      
 
         if (binario.length() > 1) { //Valida si hay algo en binario despues de %
             binario = binario.substring(1); //Le quita el primer caracter (%) y reemplaza la misma variable
@@ -68,7 +91,7 @@ public class Metodos {
         return matcher.matches(); //Retorna el decimal 
         } //Fin de la funcion para comprobar decimal
     
-    //Funcion para convertir de un operando a un decimal 
+    //Funcion para convertir de un operando binario a un decimal 
     public static String ConvertBinarioDecimal(String binario) {
         if(IsBinario(binario)) {
             int decimal = 0; //Variable auxiliar
@@ -78,20 +101,72 @@ public class Metodos {
             // Invierte la cadena binaria
             StringBuilder binarioReverso = new StringBuilder(binario).reverse();
 
-            for (int i = 0; i < longitud-1; i++) { 
-                // Obtiene el dígito en la posición i y conviértelo a entero
+            for (int i = 0; i < longitud-1; i++) {  //Obtiene el dígito en la posición i y conviértelo a entero              
                 int digito = Character.getNumericValue(binarioReverso.charAt(i)); //Obtener valores de binario de manera inversa
 
-                // Multiplica el dígito por 2 elevado a la potencia correspondiente
                 decimal += digito * Math.pow(2, i); //Elevar a la potencia 2 conforme avanza de posicion 
                 } //Fin de for 
             String DecimalString = Integer.toString(decimal); //Convertir decimal a String 
-            return DecimalString; 
+            return DecimalString; //Retornar el valor en decimal 
         } //Fin de if
         else { //Si no es binario, entonces muestra mensaje de error 
             return "Error Operando Binario"; //Mensaje de error
         } //Fin de else       
-    } //Fin del metodo  
+    } //Fin del metodo para convertir de binario a decimal 
+    
+    //Funcion para convertir de octal a Decimal
+    public static String ConvertOctalDecimal(String octal) {
+        if (IsOctal(octal)) {
+            int decimal = 0; //Variable auxiliar
+            octal = octal.substring(1); // Quita el primer caracter ("@")
+          
+            StringBuilder octalReverso = new StringBuilder(octal).reverse(); // Invierte la cadena octal
+
+            for (int i = 0; i < octal.length(); i++) { //Obtiene el dígito en la posición i y conviértelo a entero
+                char digito = octalReverso.charAt(i); //Obtener valores de octal de manera inversa
+               
+                int valorDecimal = Character.getNumericValue(digito); // Convierte el caracter octal a un valor decimal
+
+                decimal += valorDecimal * Math.pow(8, i); // Multiplica el valor decimal por 8 elevado a la potencia correspondiente
+            } //Fin de for 
+
+            String decimalString = Integer.toString(decimal);
+            return decimalString;
+        } //Fin if 
+        else {
+            return "Error, el numero ingresado no es octal"; //Mensaje de error 
+        } //Fin de else 
+    } //Fin de la funcion para convertir de octal a decimal 
+    
+    //Funcion para convertir de un operando hexadecimal a un decimal
+    public static String ConvertHexadecimalDecimal(String hexadecimal) {
+        if (IsHexadecimal(hexadecimal)) {
+            int decimal = 0; //Variable auxiliar para comparar
+            hexadecimal = hexadecimal.substring(1); // Quita el primer caracter ("$")
+
+            StringBuilder hexadecimalReverso = new StringBuilder(hexadecimal).reverse(); // Invierte la cadena hexadecimal
+
+            for (int i = 0; i < hexadecimal.length(); i++) { // Obtiene el dígito en la posición i y conviértelo a entero 
+                char digito = hexadecimalReverso.charAt(i); //Obtener valores de hexadecimal de manera inversa
+
+                // Convierte el caracter hexadecimal a un valor decimal
+                int valorDecimal; 
+                if (Character.isDigit(digito)) { 
+                    valorDecimal = Character.getNumericValue(digito);
+                } //Fin de if 
+                else {
+                    valorDecimal = Character.toUpperCase(digito) - 'A' + 10; // A=10, B=11, ..., F=15
+                } //FIn de else 
+                
+                decimal += valorDecimal * Math.pow(16, i); // Multiplica el valor decimal por 16 elevado a la potencia correspondiente
+            } //Fin de for 
+
+            String decimalString = Integer.toString(decimal); //Convertir decimal a String
+            return decimalString; //Retornar el valor en decimal 
+        } //Fin de if 
+        else { //Si no es hexadecimal, entonces muestra mensaje de error
+            return "Error, el numero ingresado no es hexadecimal"; //Mensaje de error 
+        } //Fin de else 
+    } //Fin de la funcion para convertir de hexadecimal a decimal 
+    
     } //Fin de la clase metodos 
-
-
